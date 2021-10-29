a409131475650b318aac2371dae080acfba31d64
starwalker00 <aaronwalkerup@gmail.com>
2021-10-29 19:26:31 +0200

    deploy script for SVGNFT.sol

---
+++ deploy/01_deploy_SVGNFT.js
@@ -0,0 +1,17 @@
+module.exports = async ({
+    getNamedAccounts,
+    deployments,
+    getChainId
+}) => {
+
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = await getChainId()
+
+    log("----------------------------------------------------")
+    const SVGNFT = await deploy('SVGNFT', {
+        from: deployer,
+        log: true
+    })    
+
+}
