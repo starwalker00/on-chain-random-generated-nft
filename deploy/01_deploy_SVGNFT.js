24488a90ee7488c03e090bf6d8606881105d2117
starwalker00 <aaronwalkerup@gmail.com>
2021-10-29 21:26:16 +0200

    SVGNFT.sol deployed and 1 NFT created

--- deploy/01_deploy_SVGNFT.js
+++ deploy/01_deploy_SVGNFT.js
@@ -17,12 +17,19 @@ module.exports = async ({
         log: true
     })    
     log(`You have deployed an NFT contract to ${SVGNFT.address}`)
-    let svg = fs.readFileSync('./image/star.svg', {encoding: 'utf-8'});
-    const SVGNFTContract = await ethers.getContractFactory('SVGNFT');
+    const SVGNFTContract = await ethers.getContractFactory('SVGNFT')
     const accounts = await hre.ethers.getSigners()
     const signer = accounts[0]
     const svgNFTInstance = new ethers.Contract(SVGNFT.address, SVGNFTContract.interface, signer)
     const networkName = networkConfig[chainId]['name']
     log(`Verify with:\n \tnpx hardhat verify --network ${networkName} ${SVGNFT.address}`)
+    log("Let's create an NFT now!")
+    let filepath = './image/star.svg'
+    let svg = fs.readFileSync(filepath, {encoding: 'utf-8'})
+    log(`We will use ${filepath} as our SVG, and this will turn into a tokenURI. `)
+    tx = await svgNFTInstance.create(svg)
+    await tx.wait(1) // wait for 1 block
+    log(`You've made your first NFT!`)
+    log(`You can view the tokenURI here ${await svgNFTInstance.tokenURI(0)}`)
 
 }
