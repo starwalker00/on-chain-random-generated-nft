04cc9e7572ddc02ac70430901c6ba05c08da04b0
starwalker00 <aaronwalkerup@gmail.com>
2021-10-29 21:20:49 +0200

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 // require("@nomiclabs/hardhat-etherscan");
-// require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle");
 // require("hardhat-gas-reporter");
 // require("solidity-coverage");
 require('hardhat-deploy');
