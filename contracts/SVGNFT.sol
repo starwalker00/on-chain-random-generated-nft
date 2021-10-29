46f585c7a2d94e152ff160c9cf578728d0bd1660
starwalker00 <aaronwalkerup@gmail.com>
2021-10-29 20:48:16 +0200

    SVGNFT.sol styling

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -15,25 +15,25 @@ contract SVGNFT is ERC721URIStorage {
         tokenCounter = 0;
     }
 
-    function create(string memory svg) public {
+    function create(string memory _svg) public {
         _safeMint(msg.sender, tokenCounter);
-        string memory imageURI = svgToImageURI(svg);
+        string memory imageURI = svgToImageURI(_svg);
         _setTokenURI(tokenCounter, formatTokenURI(imageURI));
         tokenCounter = tokenCounter + 1;
-        emit SVGNFTCreated(msg.sender, tokenCounter, svg);
+        emit SVGNFTCreated(msg.sender, tokenCounter, _svg);
     }
 
-    function svgToImageURI(string memory svg) public pure returns (string memory) {
+    function svgToImageURI(string memory _svg) public pure returns (string memory) {
         // transforms:
         // <svg width='500' height='500' viewBox='0 0 285 350' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill='black' d='M150,0,L75,200,L225,200,Z'></path></svg>
         // to:
         // data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nNTAwJyBoZWlnaHQ9JzUwMCcgdmlld0JveD0nMCAwIDI4NSAzNTAnIGZpbGw9J25vbmUnIHhtbG5zPSdodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2Zyc+PHBhdGggZmlsbD0nYmxhY2snIGQ9J00xNTAsMCxMNzUsMjAwLEwyMjUsMjAwLFonPjwvcGF0aD48L3N2Zz4=
         string memory baseURL = "data:image/svg+xml;base64,";
-        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(_svg))));
         return string(abi.encodePacked(baseURL,svgBase64Encoded));
     }
 
-    function formatTokenURI(string memory imageURI) public pure returns (string memory) {
+    function formatTokenURI(string memory _imageURI) public pure returns (string memory) {
         return string(
                 abi.encodePacked(
                     "data:application/json;base64,",
@@ -43,7 +43,7 @@ contract SVGNFT is ERC721URIStorage {
                                 '{"name":" "SVG NFT"', 
                                 '"description":"An NFT based on SVG!"', 
                                 '"attributes":""', 
-                                '"image":"',imageURI,'"}'
+                                '"image":"',_imageURI,'"}'
                             )
                         )
                     )
