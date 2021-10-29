d7312f5c6c0406952b66030fec46e9d8e614568e
starwalker00 <aaronwalkerup@gmail.com>
2021-10-29 19:06:15 +0200

    contract SVGNFT

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -2,9 +2,52 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "base64-sol/base64.sol";
 
 contract SVGNFT is ERC721URIStorage {
-    constructor() ERC721("SVGNFT", "SNFT") {
-        
+    // State variables
+    uint256 public tokenCounter;
+
+    // Events
+    event SVGNFTCreated(address owner, uint256 indexed tokenId, string tokenURI);
+
+    constructor() ERC721("SVG NFT", "SNFT") {
+        tokenCounter = 0;
     }
+
+    function create(string memory svg) public {
+        _safeMint(msg.sender, tokenCounter);
+        string memory imageURI = svgToImageURI(svg);
+        _setTokenURI(tokenCounter, formatTokenURI(imageURI));
+        tokenCounter = tokenCounter + 1;
+        emit SVGNFTCreated(msg.sender, tokenCounter, svg);
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        // transforms:
+        // <svg width='500' height='500' viewBox='0 0 285 350' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill='black' d='M150,0,L75,200,L225,200,Z'></path></svg>
+        // to:
+        // data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nNTAwJyBoZWlnaHQ9JzUwMCcgdmlld0JveD0nMCAwIDI4NSAzNTAnIGZpbGw9J25vbmUnIHhtbG5zPSdodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2Zyc+PHBhdGggZmlsbD0nYmxhY2snIGQ9J00xNTAsMCxMNzUsMjAwLEwyMjUsMjAwLFonPjwvcGF0aD48L3N2Zz4=
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseURL,svgBase64Encoded));
+    }
+
+    function formatTokenURI(string memory imageURI) public pure returns (string memory) {
+        return string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":" "SVG NFT"', 
+                                '"description":"An NFT based on SVG!"', 
+                                '"attributes":""', 
+                                '"image":"',imageURI,'"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }    
 }
